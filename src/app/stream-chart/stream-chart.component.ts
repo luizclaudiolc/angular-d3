ae7f313cd7d9213c68692a5f567b910fbaebbf59
Luiz Claudio <luiz.claudiolc@outlook.com>
2022-01-26 19:45:46 -0300

    adiciona eixo X, precisa corrigir as margens

--- src/app/stream-chart/stream-chart.component.ts
+++ src/app/stream-chart/stream-chart.component.ts
@@ -10,7 +10,7 @@ export class StreamChartComponent implements OnInit {
   private svg: any;
   private width = 770;
   private height = 500;
-  private margin = { top: 50, bottom: 50, left: 100, right: 20 };
+  private margin = { top: 50, bottom: 50, left: 70, right: 20 };
   private dataset = d3.json('https://raw.githubusercontent.com/luizclaudiolc/angular-d3/master/src/data/data-team/data.json');
 
   private streamChartDraw(): void {
@@ -18,76 +18,178 @@ export class StreamChartComponent implements OnInit {
 
     this.svg = d3.select('#stream')
       .append('svg')
-      .style('background-color', '#FAF6F8')
+      // .style('background-color', '#FAF6F8')
       .attr('width', this.width)
       .attr('height', this.height);
 
-    const pathCotainer = this.svg.append('g');
+    const pathContainer = this.svg.append('g');
+    const legendContainer = this.svg.append('g');
+    const xAxisContainer = this.svg.append('g')
+      .attr('id', 'xAxis')
+      .attr('transform', `translate(0, ${this.height - this.margin.bottom})`);
 
-      const menuSelect = d3.select('#stream')
-        .append('select');
+    const menuSelect = d3.select('#stream')
+      .append('select');
 
-      function mainData(dataset: any) {
-        const options = menuSelect.selectAll('options')
-          .data(Object.keys(dataset))
-          .join(
-            enter => enter.append('option'),
-            update => update,
-            exit => exit.remove()
-          )
-          .attr('value', d => d)
-          .text(d => d);
+    function mainData(dataset: any) {
+      const options = menuSelect.selectAll('options')
+        .data(Object.keys(dataset))
+        .join(
+          enter => enter.append('option'),
+          update => update,
+          exit => exit.remove()
+        )
+        .attr('value', d => d)
+        .text(d => d);
 
-        menuSelect.on('change', () => {
-          changeDraw(dataset[menuSelect.select('option:checked').text()]);
-        });
-
+      menuSelect.on('change', () => {
         changeDraw(dataset[menuSelect.select('option:checked').text()]);
-      };
+      });
 
-      const changeDraw =  (data: any) => {
-        console.log(data);
+      changeDraw(dataset[menuSelect.select('option:checked').text()]);
+    };
 
-        const stack = d3.stack().keys(Object.keys(data[0]))
-          .order(d3.stackOrderReverse)
-          .offset(d3.stackOffsetSilhouette);
+    const changeDraw =  (data: any) => {
+      console.log(data);
 
-        const stackData = stack(data);
+    const stack = d3.stack().keys(Object.keys(data[0]))
+      .order(d3.stackOrderReverse)
+      .offset(d3.stackOffsetSilhouette);
+
+    const stackData = stack(data);
+    
+    const minValue = d3.min(stackData, d=>d3.min(d, d=>d3.min(d)));
+    const maxValue = d3.max(stackData, d=>d3.max(d, d=>d3.max(d)));
+
+    const scaleX = d3.scaleLinear()
+      .domain([0, data.length -1])
+      .range([this.margin.left, this.width - this.margin.right]);
+
+    const scaleY = d3.scaleLinear()
+      .domain([minValue, maxValue] as [number, number])
+      .range([this.height - this.margin.bottom, this.margin.top]);
+
+    const colors = d3.schemePastel2;
+
+    const axis = d3.axisBottom(scaleX)
+    axis.ticks(data.length)
+      .tickFormat((d: any) => `CW${d}`)
+      .tickSize(-this.height + this.margin.top + this.margin.bottom);
+    
+    
+    
+    xAxisContainer.call(axis)
+      .call((g: any) => g.select('.domain').remove()) // serve para remover o eixo do domínio
+      .selectAll('line')
+      .attr('stroke', '#777')
+      .attr('stroke-dasharray', '5,2')
+      .attr('stroke-opacity', 0.5);
+
+
+    const area = d3.area()
+      .x((d: any, i: number) => scaleX(i))
+      .y0(d => scaleY(d[0]))
+      .y1(d => scaleY(d[1]))
+      .curve(d3.curveCatmullRomOpen);
+
+    const markes = this.svg.selectAll('circle')
+      .data(d3.range(stackData.length + 1))
+      .join(
+        (enter: any) => enter.append('circle'),
+        (update: any) => update,
+        (exit: any) => exit.remove()
+      )
+      .attr('fill', 'transparent')
+      .attr('r', 5)
+      .attr('cx', 10)
+      .attr('cy', 10)
+
+    const updateLabels = (event: any) => {
+      const ix = Math.round(scaleX.invert(event.x));
+      const text = Object.keys(data[ix]);
+      console.log(ix);
+      const markersNodes = d3.selectAll('circle').nodes();
+      
+      for (let i = 0; i < text.length; i++) {
+          d3.select(`#label-${text[i]}`)
+            .text(`${text[i]}: ${data[ix][text[i]]}`)
         
-        const minValue = d3.min(stackData, d => d3.min(d, d => d3.min(d)));
-        const maxValue = d3.max(stackData, d => d3.max(d, d => d3.max(d)));
-
-        const scaleX = d3.scaleLinear()
-          .domain([0, data.length -1])
-          .range([this.margin.left, this.width - this.margin.right]);
-
-        const scaleY = d3.scaleLinear()
-          .domain([minValue, maxValue] as [number, number])
-          .range([this.margin.bottom, this.height - this.margin.top]);
-
-        const colors = d3.schemePastel2;
-
-        const area = d3.area()
-          .x((d: any, i: number) => scaleX(i))
-          .y0(d => scaleY(d[0]))
-          .y1(d => scaleY(d[1]))
-          .curve(d3.curveCatmullRomOpen);
-
-        const paths = pathCotainer.selectAll('path')
-          .data(stackData)
-          .join(
-            (enter: any) => enter.append('path'),
-            (update: any) => update,
-            (exit: any) => exit.remove()
-          )
-          .transition()
-          .duration(1000)
-          .attr('d', area)
-          .attr('stroke', '#cece')
-          .attr('fill', (d: any, i: number) => colors[i])
-          .attr('fill-opacity', 0.75)
-          .attr('id', (d: any, i: number) => `path-${Object.keys(data[0])[i]}`);
+          // markers update
+          d3.select(markersNodes[i])
+            .attr('cx', scaleX(ix))
+            .attr('cy', scaleY(stackData[i][ix][0]))
+            .attr('fill', 'orange')
       };
+
+      d3.select(markersNodes[text.length])
+        .attr('cx', scaleX(ix))
+        .attr('cy', scaleY(stackData[0][ix][1]));
+    };
+
+    const resetLabels = (event: any) => {
+      const markersNodes = d3.selectAll('circle').nodes();
+      const text = Object.keys(data[0]);
+      for (let i of text) {
+        d3.select(`#label-${i}`)
+        .text(`${i}`);
+      };
+
+      d3.selectAll(markersNodes)
+        .transition()
+        .duration(500)
+        .attr('fill', 'transparent');
+    };
+
+    const paths = pathContainer.selectAll('path')
+      .data(stackData)
+      .join(
+        (enter: any) => enter.append('path'),
+        (update: any) => update,
+        (exit: any) => exit.remove()
+      )
+      .on('mousemove', updateLabels)
+      .on('mouseleave', resetLabels)
+      .transition()
+      .duration(1000)
+      .attr('d', area)
+      .attr('stroke', '#cece')
+      .attr('fill', (d: any, i: number) => colors[i])
+      .attr('fill-opacity', 0.75)
+      .attr('id', (d: any, i: number) => `path-${Object.keys(data[0])[i]}`);
+
+
+      const rect = legendContainer.selectAll('rect')
+      .data(Object.keys(data[0]))
+      .join(
+        (enter: any) => enter.append('rect'),
+        (update: any) => update,
+        (exit: any) => exit.remove()
+      )
+      .attr('width', 15)
+      .attr('height', 15)
+      .attr('stroke', '#cece')
+      .attr('fill', (d: any, i: number) => colors[i])
+      .attr('fill-opacity', 0.75)
+      .attr('x', 20)
+      .attr('y', (d: any, i: number) => (this.height / 2) / 2 + i * 25);
+
+      const legend = legendContainer.selectAll('text')
+      .data(Object.keys(data[0]))
+      .join(
+        (enter: any) => enter.append('text'),
+        (update: any) => update,
+        (exit: any) => exit.remove()
+      )
+      .attr('fill', 'gray')
+      .attr('alignment-baseline', 'hanging')
+      .attr('x', 40)
+      .attr('y', (d: any, i: number) => (this.height / 2) / 2 + i * 25)
+      .text((d: any) => d)
+      .attr('class', 'label')
+      .attr('id', (d: any, i: any) => `label-${d}`);
+      
+      // legend.text((d: any) => d);
+    };
   };
 
 
