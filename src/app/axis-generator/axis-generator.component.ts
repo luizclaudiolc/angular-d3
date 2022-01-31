d0f7a18f758bb3d9079da92dfc1e2fff62c1a032
Luiz Claudio <luiz.claudiolc@outlook.com>
2022-01-31 08:23:57 -0300

    refactor axis generator and add new default

--- src/app/axis-generator/axis-generator.component.ts
+++ src/app/axis-generator/axis-generator.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Component, Inject, OnInit } from '@angular/core';
 import * as d3 from 'd3';
 
 @Component({
@@ -7,13 +8,19 @@ import * as d3 from 'd3';
   styleUrls: ['./axis-generator.component.scss']
 })
 export class AxisGeneratorComponent implements OnInit {
-  private margin = { top: 40, right: 40, bottom: 30, left: 50 };
-  private width = 750;
-  private height = 400;
-  private data = {
+  svg: any;
+  margin = { top: 40, right: 40, bottom: 30, left: 50 };
+  width = 750;
+  height = 400;
+  scaleX: any;
+  scaleY: any;
+  dots: any;
+  lineLength!: number;
+  color = d3.scaleOrdinal(d3.schemeCategory10);
+  data = {
     monthAndPercentage: [
-      { month: 'Jan', percentage: 95, year: 21 },
-      { month: 'Fev', percentage: 45, year: 21 },
+      { month: 'Jan', percentage: 15, year: 21 },
+      { month: 'Fev', percentage: 75, year: 21 },
       { month: 'Mar', percentage: 35, year: 21 },
       { month: 'Abr', percentage: 50, year: 21 },
       { month: 'Mai', percentage: 11, year: 21 },
@@ -24,135 +31,174 @@ export class AxisGeneratorComponent implements OnInit {
       { month: 'Out', percentage: 100, year: 21 },
       { month: 'Nov', percentage: 15, year: 21 },
       { month: 'Dez', percentage: 66, year: 21 },
-      { month: 'Jan', percentage: 12, year: 22 },
-      { month: 'Fev', percentage: 45, year: 22 },
-      { month: 'Mar', percentage: 35, year: 22 },
+      { month: 'Jan', percentage: 3, year: 22 },
+      // { month: 'Fev', percentage: 45, year: 22 },
+      // { month: 'Mar', percentage: 35, year: 22 },
+      // { month: 'Abr', percentage: 50, year: 22 },
+      // { month: 'Mai', percentage: 11, year: 22 },
+      // { month: 'Jun', percentage: 60, year: 22 },
+      // { month: 'Jul', percentage: 65, year: 22 },
+      // { month: 'Ago', percentage: 50, year: 22 },
+      // { month: 'Set', percentage: 35, year: 22 },
+      // { month: 'Out', percentage: 100, year: 22 },
+      // { month: 'Nov', percentage: 15, year: 22 },
+      // { month: 'Dez', percentage: 66, year: 22 }
     ]
   };
+  constructor(@Inject(DOCUMENT) private document: Document) { }
+  
+  ngOnInit(): void {
+    this.drawChart();
+    this.chartUpdate();
+    this.lineAnimation();
+    this.dotsAnimation();
+    this.mouseEvents();
+  }
 
-  private drawAxis(): void {
-    const svg = d3.select('#axis')
+  ngAfterViewInit(): void {
+  };
+
+  drawChart(): void {
+    this.svg = d3.select('#axis')
       .append('svg')
       // .style('background-color', '#F6F8FA')
+      .attr('id', 'line-chart')
       .attr('width', this.width)
       .attr('height', this.height);
-    
-    const scaleX = d3.scalePoint() 
+
+    d3.select('svg#line-chart').append('g').attr('id', 'line-g-x-axis');
+    d3.select('svg#line-chart').append('g').attr('id', 'line-g-y-axis');
+    d3.select('svg#line-chart').append('g').attr('id', 'line-g-line-path');
+    d3.select('svg#line-chart').append('g').attr('id', 'line-g-dots');
+
+    d3.select('g#line-g-line-path').append('path').attr('id', 'line-path');
+  };
+
+  chartUpdate(): void {
+    if (!this.data.monthAndPercentage.length) return;
+
+    // update svg dimensions
+    d3.select('svg#line-chart')
+      .attr('width', this.width)
+      .attr('height', this.height);
+
+    // ****** Scales ****** //
+    this.scaleX = d3.scalePoint()
       .domain(this.data.monthAndPercentage.map(d => `${d.month}/${d.year}`))
       .range([this.margin.left, this.width - this.margin.right]);
 
-    const scaleY = d3.scaleLinear()
-      .domain([0, d3.max(this.data.monthAndPercentage, d => d.percentage)] as [number, number])
-      .range([this.height - this.margin.bottom, this.margin.top]);
+    this.scaleY = d3.scaleLinear()
+    .domain([0, d3.max(this.data.monthAndPercentage, d => d.percentage)] as [number, number])
+    .range([this.height - this.margin.bottom, this.margin.top]);
 
-    const axisX = d3.axisBottom(scaleX)
-      // .tickSize(5)
-      .tickSizeOuter(0);
-    
-    const axisY = d3.axisLeft(scaleY)
-      .ticks(10)
-      /* Serve para fazer logicas de personalização do seu eixo */
-      .tickFormat((d) => `${d} %`);
-      // serve para, se necessário, alterar o valor do eixo
-      // .tickValues([15, 100, 200, 300, 400, 500, 600, 700, d3.max(this.dataset) as number]); 
+    // ****** X-Axis ****** //
+    const xAxis = d3.axisBottom(this.scaleX)
+      // .ticks(this.width / 80) // verificar se é necessário
+      // .tickFormat(d => d.)̣
+      .tickSizeOuter(0)
+      // .tickSizeInner(5);
 
-    const gridY = d3.axisLeft(scaleY)
-      .tickSize(-this.width + this.margin.left + this.margin.right)
-      .tickFormat(() => '')
-      .tickSizeOuter(0);
+    d3.select('#line-g-x-axis')
+      .call(xAxis as any)
+      .attr('transform', `translate(0, ${this.height - this.margin.bottom})`);
 
-    svg.append('g')
-      .attr('transform', `translate(0, ${this.height - this.margin.bottom})`)
-      .call(axisX);
-      // .attr('transform', 'rotate(-15)')
+    // ****** Y-Axis ****** //
+    const yAxis = d3.axisLeft(this.scaleY)
+      .ticks(this.height / 80) // verificar se é necessário
+      .tickSize(this.margin.left + this.margin.right - this.width)
+      .tickSizeOuter(0)
+      .tickFormat((d) => `${d}%`);
 
-    svg.append('g')
-      .attr('transform', `translate(${this.margin.left}, ${0})`)
-      .call(axisY);
+    d3.select('g#line-g-y-axis')
+      .call(yAxis as any)
+      .attr('transform', `translate(${this.margin.left}, 0)`)
+      .selectAll('line')
+      .attr('stroke', 'gray')
+      .attr('stroke-dasharray', '10,5')
+      .attr('opacity', 0.1);
 
-    svg.append('g')
-    .attr('transform', `translate(${this.margin.left}, ${0})`)
-    .call(gridY)
-    .selectAll('line')
-    .attr('stroke', (d) => d === 0 ? 'none' : '#cece')
-    .attr('stroke-dasharray', '5,2')
-    .attr('stroke-opaicty', 0.1)
-    .attr('stroke-width', 1.5);
+    d3.select('#line-g-y-axis').select('path.domain').attr('stroke', 'none');
 
-    const line = d3.line()
+    // ****** Line ****** //
+      const generatorLine = d3.line()
       .x((d: any) => d[0])
       .y((d: any) => d[1])
       .curve(d3.curveCardinal.tension(0.5));
 
-    svg.append('g')
-      .append('path')
-      .attr('d', line(this.data.monthAndPercentage.map(d => [scaleX(`${d.month}/${d.year}`), scaleY(d.percentage)] as [number, number])))
-      .attr('stroke-dasharray', 2000) // preciso pegar o getTotalLength() para melhorar essa função
-      .attr('stroke-dashoffset', 2000)
-      .transition()
-      .duration(800)
-      .ease(d3.easeExpIn)
-      .attr('stroke-dashoffset', 0)
-      .attr('stroke', '#000')
-      .attr('stroke-width', 1.5)
-      .attr('fill', 'none');
+      const linePath = d3.select('#line-path')
+        .attr('d', generatorLine(this.data.monthAndPercentage.map(d => [this.scaleX(`${d.month}/${d.year}`),
+          this.scaleY(d.percentage)])))
+        .attr('fill', 'none')
+        .attr('stroke', (d: any) => `${this.color(d)}`)
+        .attr('stroke-width', 2);
+        
+      // get line length
+      this.lineLength = this.document.querySelector<any>('#line-path')?.getTotalLength();
 
-    svg.append('g')
-      .selectAll('circle')
-      .data(this.data.monthAndPercentage)
-      .join(
-        (enter: any) => enter.append('circle'),
-        (update: any) => update,
-        (exit: any) => exit.remove()
-      )
-      .attr('id', (_d: any, i: any) => `circle-${i}`)
-      .attr('cx', (d: any) => scaleX(`${d.month}/${d.year}`) as any)
-      .attr('cy', (d: any) => scaleY(d.percentage))
-      .attr('r', 5)
-      .attr('fill', '#F6F8FA')
-      .attr('stroke', '#000');
-
-    function mouseenter (this: any) {
-      d3.select(this)
-        .transition()
-        .duration(75)
-        .style('cursor', 'pointer')
-        .attr('r', 7.5)
-        .attr('stroke', '#000')
-        .attr('stroke-width', 1.5);
-    };
-    function mouseleave (this: any) {
-      d3.select(this)
-        .transition()
-        .duration(75)
+      // ****** Dots ****** //
+      this.dots = d3.select('g#line-g-dots')
+        .selectAll('circle')
+        .data(this.data.monthAndPercentage)
+        .join(
+          enter => enter.append('circle'),
+          (update: any) => update,
+          exit => exit.remove()
+        )
+        .attr('id', (_d: any, i: any) => `circle-${i}`)
+        .attr('cx', (d: any) => this.scaleX(`${d.month}/${d.year}`) as any)
+        .attr('cy', (d: any) => this.scaleY(d.percentage))
         .attr('r', 5)
-        .attr('stroke', '#000')
-        .attr('stroke-width', 1);
-    };
-    function click (this: any) {
-      d3.select(this)
-        .transition()
-        .duration(75)
-        .attr('r', 10)
-        .attr('stroke', '#000')
-        .attr('stroke-width', 1.5)
-        .transition()
-        .duration(75)
-        .attr('r', 7.5)
-        .attr('stroke', '#000')
-        .attr('stroke-width', 1);
-    }
-
-    d3.selectAll('circle')
-      .on('mouseenter', mouseenter)
-      .on('mouseleave', mouseleave)
-      .on('click', click);
+        .attr('fill', '#F6F8FA')
+        .attr('stroke', (d: any) => `${this.color(d)}`);
   };
 
-  constructor() { }
+  // ****** line Animation ****** //
+  lineAnimation(): void {
+    d3.select('#line-path')
+      .attr('stroke-dasharray', this.lineLength * 10)
+      .attr('stroke-dashoffset', this.lineLength * 10)
+      .transition()
+      .duration(1400)
+      .ease(d3.easeExpIn)
+      .attr('stroke-dashoffset', 0);
 
-  ngOnInit(): void {
-    this.drawAxis();
-  }
+    this.chartUpdate();
+  };
 
+  // ****** dots Animation ****** //
+  dotsAnimation(): void {
+    this.dots
+      .attr('r', 0)
+      .transition()
+      .delay((d: any, i: number, arr: any) => (i * 800) / arr.length + 400)
+      .attr('r', 5);
+  };
+
+  // ****** mouseEvents ****** //
+  mouseEvents(): void {
+    const { color } = this;
+    
+    d3.selectAll('circle')
+      .on('mousemove', function () {
+        d3.select(this)
+          .attr('r', 7.5)
+          .attr('stroke', (d: any) => `${color(d)}`)
+          .attr('stroke-width', 2)
+          .attr('cursor', 'pointer')
+      })
+      .on('mouseleave', function () {
+        d3.select(this)
+          .attr('r', 5)
+          .attr('stroke-width', 1)
+          .attr('stroke', (d: any) => `${color(d)}`);
+      })
+      .on('click', function () {
+        d3.select(this)
+          .attr('r', 10)
+          .attr('stroke', (d: any) => `${color(d)}`)
+          .transition()
+          .attr('r', 5)
+      });
+  };
+  
 }
